6447e6f21f365b01bd2a391653f99ced5cc5ef9c
Akram-00 <aliakram9789@gmail.com>
2025-03-24 17:29:04 +0530

    community data is added

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+interface CommunityGroup {
+    title: string;
+    subtitle: string;
+    description: string;
+    ctaBtnTitle: string;
+    trendingDiscussions: string[];
+    imgSrc:string;
+    expansion?:string;
+  }
