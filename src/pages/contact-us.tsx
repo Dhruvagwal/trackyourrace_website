169b7667fff28db05f29f00e76269df5e8bf4351
dhruv aggarwal <dhruvgwal@gmail.com>
2025-03-24 14:00:46 +0530

    starteer

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "@/components/main/navbar";
+import Image from "next/image";
 function ContactUs() {
   return (
-    <div>
-      
+    <div className="px-main relative max-md:px-main-md">
+      <Navbar />
+      {/* orb */}
+      <div className="blur-[200px] absolute top-[-12rem] right-[-12rem] bg-primary h-96 w-96 rounded-full" />
+      <div className="z-1 mt-12 relative">
+        <Image
+          className="object-cover h-[50vh] w-full rounded-3xl"
+          width={1500}
+          height={500}
+          alt="event"
+          src="https://images.unsplash.com/photo-1596460658047-1826d5921c56?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
