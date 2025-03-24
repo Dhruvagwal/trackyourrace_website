9c6e8997828946b8c2a6ac18fc32749a450d53d4
dhruv aggarwal <dhruvgwal@gmail.com>
2025-03-24 13:54:16 +0530

    starter

--- src/components/main/event-card.tsx
+++ src/components/main/event-card.tsx
@@ -15,14 +15,14 @@ function EventCard({ title, date, location, image, id }: IEvent) {
     <div className="bg-secondary rounded-2xl w-full p-4">
       <div className="relative">
         <Image
-          className="card-mask object-cover h-[22rem] md:h-[24rem]"
+          className="card-mask object-cover h-[19rem] md:h-[24rem]"
           src={image}
           width={500}
           height={500}
           alt={title}
         />
           <Link href={`/events/${id}`}>
-        <Button asChild className="rounded-full w-14 h-14 bottom-0 -right-2 absolute ">
+        <Button asChild className="rounded-full md:w-14 w-12 h-12 md:h-14 bottom-0 -right-2 absolute ">
             <ArrowUpRight />
         </Button>
           </Link>
